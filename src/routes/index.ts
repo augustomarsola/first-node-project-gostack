832795d79607f578fb6e341ad5cc243dfe4fd52a
augustomarsola <augusto.marsola@live.com>
2020-06-22 13:04:23 -0300

    Iniciando repositório com todas as configurações

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+// src/routes/index.ts
+
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
+export default routes;
