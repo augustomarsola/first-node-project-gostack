fee46bb2ca167d2ab606d2f8bd6df18a7b7c9d7b
augustomarsola <augusto.marsola@live.com>
2020-06-26 16:43:27 -0300

    📚 Completed de middleware to authenticate

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '5c43f691fd262ac979703712fffee10b',
+    expiresIn: '1d',
+  },
+};
---
+++ src/database/migrations/1593200561990-AddAvatarFieldToUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddAvatarFieldToUsers1593200561990 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
