c77606ab6205fb9cfe29c8aad99023f91975a377
augustomarsola <augusto.marsola@live.com>
2020-06-26 11:41:38 -0300

    📚 Completed the user authenticate service

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,10 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination!');
     }
 
-    // Usuário auntenticado
+    const token = sign({}, '5c43f691fd262ac979703712fffee10b', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
     return {
       user,
+      token,
     };
   }
 }
