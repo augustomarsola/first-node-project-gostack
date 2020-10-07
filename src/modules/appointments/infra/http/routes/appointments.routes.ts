092b4f1bb272709e9aa3b8289b67c0597dbe8b80
augustomarsola <augusto.marsola@live.com>
2020-10-07 10:51:44 -0300

    📚 Joi validation on Routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-
 import AppointmentController from '../controller/AppointmentsController';
 import ProviderAppointmentsController from '../controller/ProviderAppointmentsController';
 
@@ -11,7 +11,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+);
 
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
